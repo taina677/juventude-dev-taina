8a57f65100b3aa999c3710f92fb650369da358c2
taina677 <tainazinhaok67@gmail.com>
2026-09-22 19:16:54 +0000

    atividade de terça 22/09

---
+++ aula_09/exemplos/teste.js
@@ -0,0 +1,13 @@
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log("Maior de idade");
+    } 
+    else if (idade==30){ 
+        console.log ("Igual a 30")}
+    else {
+    
+        console. log("Igual a 30")
+    }
+}
+
+console.log(verificarIdade(30))
